d0fd900711e389b38dbca66df39a75fc05794b46
Francesco <francescococlite@gmail.com>
2021-07-24 20:00:43 +0200

    fine esercizio

--- badwords.php
+++ badwords.php
@@ -1,3 +1,12 @@
+<?php
+    $testo = "Lorem ipsum dolor sit amet, consectetur adipisicing elit";
+    $lunghezza= strlen($testo);
+    $censura= $_GET['censura'];
+    $rimpiazzo= str_replace($censura, '***', $testo)
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,5 +17,18 @@
 </head>
 <body>
     
+<!-- Creare una variabile con un paragrafo di testo a vostra scelta. Stampare a schermo il paragrafo e la sua lunghezza. Una parola da censurare viene passata dall'utente tramite parametro GET. Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+
+<p> <?php echo $testo ?>
+
+<br> e la lunghezza della stringa è: <?php echo $lunghezza ?>
+
+</p>
+<?php echo $rimpiazzo ?>
+<p>
+    
+</p>
+
+
 </body>
 </html>
